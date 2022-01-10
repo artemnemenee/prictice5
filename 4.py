a701a65246e082c35be4aa0a9a8cf7377415b206
Artem Petrochenko <petrochenko.av@edu.spbstu.ru>
2022-01-10 18:05:35 +0300

    4, 5 new

--- 4.py
+++ 4.py
@@ -1,22 +1,20 @@
-def a(tex):
-    count = 0
-    for i in tex:
-        if gl.find(i) != -1:
-            count += 1
-    return count
+import random
+def quiz():
+    from random import shuffle
+    dictionary = {
+        'Russia': 'Moscow',
+        'Belarus': 'Minsk',
+        'Ukraine': 'Kiev'}
+    random = list(dictionary.keys())
+    shuffle(random)
+    score = 0
 
-def b(tex):
-    count = 0
-    for i in tex:
-        if sogl.find(i) != -1:
-            count += 1
-    return count
-
-tex = str(input("Текст: "))
-gl = "Аа, Ее, Ёё, Ии, Оо, Уу, Ыы, Ээ, Юю, Яя"
-sogl = "Бб, Вв, Гг, Дд, Жж, Зз, Йй, Кк, Лл, Мм, Нн, Пп, Рр, Сс, Тт, Фф, Хх, Чч, Цц, Шш, Щщ"
-choice = str(input("согласная(с) или гласная(г): "))
-if choice == "с":
-    print(b(tex))
-elif choice == "г":
-    print(a(tex))
+    for i in random:
+        answer = input(f'Enter the capital of {i}: ')
+        if answer == dictionary[i]:
+            score += 100
+            print(f'Correct answer, your score {score}')
+        else:
+            print(f'Wrong answer, your score {score}')
+    print(f'Final score {score}')
+quiz()
